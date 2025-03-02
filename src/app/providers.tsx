02db72f8018c23e9615764517f7fd88c92d8e29b
Mohamed Faizel <mm00894227.techmahindra.com@partner.airbus.com>
2025-03-02 11:57:41 +0530

    Auth - Github

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,15 @@
 'use client';
 import {HeroUIProvider} from "@heroui/react";
-
+import { SessionProvider } from 'next-auth/react';
 interface ProviderProps {
     children: React.ReactNode
 }
 
 function Providers({children}:ProviderProps) {
   return (
+    <SessionProvider>
     <HeroUIProvider>{children}</HeroUIProvider>
+    </SessionProvider>
   )
 }
 
